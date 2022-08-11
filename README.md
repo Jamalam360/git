b5115b8660c7db1f5dc44955f12708dd6536dd1f
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-08-11 13:39:20 +0100

    feat: initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# GitHub Link
+
+This script powers `git.jamalam.tech`, which I use to link to repositories on
+GitHub under my account, or any organizations it is in.
+
+## Setup
+
+The project can be deployed quickly on Deno Deploy using [this link](). The
+following environment variables must be set:
+
+- `GITHUB_USERS`: A comma-separated list of GitHub users & organisations to link
+  to.
+- `PORT`: The port to run the server on, defaults to 8000.
---
+++ main.ts
@@ -0,0 +1,61 @@
+import { serve } from "https://deno.land/std@0.151.0/http/mod.ts";
+import { config } from "https://deno.land/std@0.151.0/dotenv/mod.ts";
+
+await config({
+    export: true,
+});
+
+const githubUsers = Deno.env.get("GITHUB_USERS")!.split(",").map((user) =>
+    user.trim()
+);
+const cache: Map<string, { time: number; url: string }> = new Map();
+
+await serve(
+    async (req) => {
+        const path = new URL(req.url).pathname;
+
+        let url = "";
+
+        if (cache.has(path)) {
+            const { time, url } = cache.get(path)!;
+
+            // Check if the cache is still valid (1 hour)
+            if (time > Date.now() - 3600000) {
+                return new Response("", {
+                    status: 302,
+                    headers: {
+                        Location: url,
+                    },
+                });
+            } else {
+                cache.delete(path);
+            }
+        }
+
+        for (const user of githubUsers) {
+            const req = await fetch(`https://github.com/${user}/${path}`);
+
+            if (req.status == 404) continue;
+
+            url = `https://github.com/${user}/${path}`;
+            cache.set(path, { time: Date.now(), url });
+            break;
+        }
+
+        if (url == "") {
+            return new Response("", {
+                status: 404,
+            });
+        } else {
+            return new Response("", {
+                status: 302,
+                headers: {
+                    Location: url,
+                },
+            });
+        }
+    },
+    {
+        port: parseInt(Deno.env.get("PORT") ?? "8000"),
+    },
+);
